c08fe09f61bc970f5024323fb07a99b09959ef29
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-12 09:14:38 +0200

    Removed empty 'new' folder and corrected DB_URL to match DATABASE_URL on aiven

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ require('dotenv').config();
 const express = require("express");
 const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 const app = express();
 
 // Middleware to parse form data (important!)
 app.use(express.urlencoded({ extended: true }));
 
 // Use the user routes
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes.js");
 app.use("/", userRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  connectionString: process.env.DB_URL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
